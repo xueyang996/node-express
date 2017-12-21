7b476e97e691f254129ba778783963d6f233690e
张明瑞 <zhangmr@shumei.ai>
2017-12-21 17:35:53 +0800

    jichu

---
+++ meadowlark.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+app.set('port', process.env.port || 3000);
+
+// 设置handlerbars视图引擎
+var handlebars = require('express3-handlebars').create({
+	defaultLayout: 'main'
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+// 404 catch-all处理器(中间件)
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+// 500 错误处理(中间件)
+app.use(function(err, req, res, next) {
+	res.status(500);
+	res.render('500');
+});
+app.listen(app.get('port'), function() {
+	console.log('Express started on http://localhost:' + app.get('port') + '; press ctrl-c to terminate');
+});
